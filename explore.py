8cc0e911ab13e1df2704a205a57f2d2b749ae6f0
Scott Barnett <scottbarnett97@gmail.com>
2023-05-09 12:02:04 -0500

    created explore files

---
+++ explore.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import seaborn as sns
+import numpy as np
+from env import user,password,host
+import os
+import wrangle
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+
+import sklearn.preprocessing
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings("ignore")
+
--- wrangle.py
+++ wrangle.py
@@ -64,6 +64,24 @@ def wrangle_zillow(df):
     return df
 
 
+def split_data(df):
+    '''
+    Be sure to code it as train, validate, test = split_data(df)
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.25, 
+                                       random_state=123, 
+                                       )
+    #This confirms and Validates my split.
+    
+    print(f'train -> {train.shape}, {round(train.shape[0]*100 / df.shape[0],2)}%')
+    print(f'validate -> {validate.shape},{round(validate.shape[0]*100 / df.shape[0],2)}%')
+    print(f'test -> {test.shape}, {round(test.shape[0]*100 / df.shape[0],2)}%')
+    
+    return train, validate, test
 '''
 ### ACQUIRE DATA ###
 
